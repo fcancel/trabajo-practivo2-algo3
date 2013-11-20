7c121b86bda48d2667d6ad37fd0584ab07ee481f
mafvidal@gmail.com <mafvidal@gmail.com@1b63ddbd-f892-29c8-733d-df3772dd4309>
2013-11-20 13:19:25 +0000

    Cambio en detalles de la ciudad

--- test/fiuba/algo3/tests/TestLasFuncionesDeLaCiudad.java
+++ test/fiuba/algo3/tests/TestLasFuncionesDeLaCiudad.java
@@ -1,6 +1,7 @@
 package fiuba.algo3.tests;
 
 import static org.junit.Assert.*;
+import fiuba.algo3.modelo.Auto;
 import fiuba.algo3.modelo.Calle;
 import fiuba.algo3.modelo.Ciudad;
 import fiuba.algo3.modelo.EstadoVehiculo;
@@ -8,6 +9,7 @@ import fiuba.algo3.modelo.GPS;
 import fiuba.algo3.modelo.Moto;
 import fiuba.algo3.modelo.Posicion;
 import fiuba.algo3.modelo.Vehiculo;
+import fiuba.algo3.modelo.excepciones.JuegoNoIniciado;
 import fiuba.algo3.modelo.excepciones.MovimientoInvalido;
 
 import org.junit.Assert;
@@ -31,18 +33,16 @@ public class TestLasFuncionesDeLaCiudad {
     }
 
 	@Test
-    public void ubicaElVehiculoEnUnaPosicionValida() throws MovimientoInvalido{
-		int dimension = 10;
-		Posicion posicion = new Posicion();
-		posicion.setX(1);
-		posicion.setY(1);
+    public void ubicaElVehiculoEnUnaPosicionValida() throws MovimientoInvalido, JuegoNoIniciado{
+		
 		GPS gps = new GPS();
-        EstadoVehiculo estado = new Moto();
-        Vehiculo vehiculo = new Vehiculo(estado);
-        Ciudad ciudad = new Ciudad(dimension,vehiculo,gps);
-        Calle calle = ciudad.calleEnUnaPosicion(posicion);
-        calle.inicializarCalle();
-        assertTrue(calle.getVehiculo() == vehiculo);
+		gps.empezarJuego(new Auto(), 5, "juan");
+		Vehiculo auto = gps.getVehiculo();
+		Ciudad ciudad = gps.getCiudad();
+		Posicion posicion = auto.getPosicion();
+		Calle calle = ciudad.calleEnUnaPosicion(posicion);
+		
+		assertTrue(calle.getVehiculo() == auto);
     }
 
 	@Test
