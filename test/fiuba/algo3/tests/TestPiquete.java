fac6413a8a0b0e13bb1086f00bc4cfb3561a3d4c
mafvidal@gmail.com <mafvidal@gmail.com@1b63ddbd-f892-29c8-733d-df3772dd4309>
2013-11-20 16:36:19 +0000

    Completada la clase piquete

--- test/fiuba/algo3/tests/TestPiquete.java
+++ test/fiuba/algo3/tests/TestPiquete.java
@@ -4,12 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import fiuba.algo3.modelo.Auto;
+import fiuba.algo3.modelo.Calle;
+import fiuba.algo3.modelo.Ciudad;
+import fiuba.algo3.modelo.CuatroPorCuatro;
 import fiuba.algo3.modelo.Efecto;
 import fiuba.algo3.modelo.GPS;
 import fiuba.algo3.modelo.Moto;
 import fiuba.algo3.modelo.Piquete;
+import fiuba.algo3.modelo.Posicion;
 import fiuba.algo3.modelo.Vehiculo;
 import fiuba.algo3.modelo.excepciones.JuegoNoIniciado;
+import fiuba.algo3.modelo.excepciones.MovimientoInvalido;
 
 public class TestPiquete {
 
@@ -28,18 +34,52 @@ public class TestPiquete {
 		assertEquals(gps.getMovimientos(),2);
 	}
 	
-	/*@Test
-	public void alCaerUnAutoEnUnPiqueteDebeVolverALaPosicionAnterior(){
+	@Test
+	public void alCaerUnAutoEnUnPiqueteDebeVolverALaPosicionAnterior() throws JuegoNoIniciado, MovimientoInvalido{
 		
+		GPS gps = new GPS();
+		gps.empezarJuego(new Auto(), 10, "juan");
+		Vehiculo auto = gps.getVehiculo();
+		Efecto piquete = new Piquete();
 		
+		// El vehiculo se ubicara en la posicion (1,3)
+		Posicion posicion = auto.getPosicion();
+		
+		assertEquals(1,posicion.getX());
+		assertEquals(3,posicion.getY());
+		
+		auto.moverDerecha();
+		
+		auto.aceptarEfecto(piquete);
+		
+		// el auto retrocede una posicion
+		assertEquals(1,posicion.getX());
+		assertEquals(3,posicion.getY());
 		
 	}
 	
 	@Test
-	public void alCaerUnaCuatroPorCuatroEnUnPiqueteDebeVolverALaPosicionAnterior(){
+	public void alCaerUnaCuatroPorCuatroEnUnPiqueteDebeVolverALaPosicionAnterior() throws JuegoNoIniciado, MovimientoInvalido{
 		
+		GPS gps = new GPS();
+		gps.empezarJuego(new CuatroPorCuatro(), 10, "juan");
+		Vehiculo cuatroPorCuatro = gps.getVehiculo();
+		Efecto piquete = new Piquete();
 		
+		// El vehiculo se ubicara en la posicion (1,3)
+		Posicion posicion = cuatroPorCuatro.getPosicion();
 		
-	}*/
+		assertEquals(1,posicion.getX());
+		assertEquals(3,posicion.getY());
+		
+		cuatroPorCuatro.moverDerecha();
+		
+		cuatroPorCuatro.aceptarEfecto(piquete);
+		
+		// la 4X4 retrocede una posicion
+		assertEquals(1,posicion.getX());
+		assertEquals(3,posicion.getY());
+		
+	}
 
 }
