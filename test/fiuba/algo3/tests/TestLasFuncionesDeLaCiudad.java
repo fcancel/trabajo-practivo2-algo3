35fd1476c0728541ef2b1c2d81aa4fdf51bf7544
federicoprandi <federicoprandi@1b63ddbd-f892-29c8-733d-df3772dd4309>
2013-11-20 04:56:25 +0000

    Actulizacion - Mejora de la ubicación del vehiculo y la meta random

--- test/fiuba/algo3/tests/TestLasFuncionesDeLaCiudad.java
+++ test/fiuba/algo3/tests/TestLasFuncionesDeLaCiudad.java
@@ -1,7 +1,6 @@
 package fiuba.algo3.tests;
 
 import static org.junit.Assert.*;
-
 import fiuba.algo3.modelo.Calle;
 import fiuba.algo3.modelo.Ciudad;
 import fiuba.algo3.modelo.EstadoVehiculo;
@@ -10,11 +9,26 @@ import fiuba.algo3.modelo.Moto;
 import fiuba.algo3.modelo.Posicion;
 import fiuba.algo3.modelo.Vehiculo;
 import fiuba.algo3.modelo.excepciones.MovimientoInvalido;
-import org.junit.Assert;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestLasFuncionesDeLaCiudad {
+
+	@Test
+    public void validarPosicion() throws MovimientoInvalido{
+		int dimension = 10;
+		Posicion posicion = new Posicion();
+		posicion.setX(1);
+		posicion.setY(1);
+		GPS gps = new GPS();
+        EstadoVehiculo estado = new Moto();
+        Vehiculo vehiculo = new Vehiculo(estado);
+        Ciudad ciudad = new Ciudad(dimension,vehiculo,gps);
+        Calle calle = ciudad.calleEnUnaPosicion(posicion);
+        calle.inicializarCalle();
+        assertTrue(ciudad.esValidaLaPosicion(posicion) == true);
+    }
 
 	@Test
     public void ubicaElVehiculoEnUnaPosicionValida() throws MovimientoInvalido{
@@ -25,12 +39,12 @@ public class TestLasFuncionesDeLaCiudad {
 		GPS gps = new GPS();
         EstadoVehiculo estado = new Moto();
         Vehiculo vehiculo = new Vehiculo(estado);
-        Ciudad ciudad = new Ciudad(dimension,vehiculo,gps);		
-        Assert.assertEquals(true, ciudad.esValidaLaPosicion(posicion));
+        Ciudad ciudad = new Ciudad(dimension,vehiculo,gps);
         Calle calle = ciudad.calleEnUnaPosicion(posicion);
+        calle.inicializarCalle();
         assertTrue(calle.getVehiculo() == vehiculo);
     }
-	
+
 	@Test
     public void alMovermeAUnLugarInvalidoReciboExcepcionMovimientoInvalido(){
         try{
@@ -39,10 +53,10 @@ public class TestLasFuncionesDeLaCiudad {
     		posicion.setX(0);
     		posicion.setY(0);
     		GPS gps = new GPS();
-            EstadoVehiculo estado = new Moto();
-            Vehiculo vehiculo = new Vehiculo(estado);
-            Ciudad ciudad = new Ciudad(dimension,vehiculo,gps);		
-            ciudad.esValidaLaPosicion(posicion);
+    		EstadoVehiculo estado = new Moto();
+    		Vehiculo vehiculo = new Vehiculo(estado);
+    		Ciudad ciudad = new Ciudad(dimension,vehiculo,gps);
+    		ciudad.esValidaLaPosicion(posicion);
         	assertTrue( false );
         	
         }catch( MovimientoInvalido e ){
