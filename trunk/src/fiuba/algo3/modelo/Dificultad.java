c8adb7791df3da8af9db4fb59dca82a0aa1f97ea
mafvidal@gmail.com <mafvidal@gmail.com@1b63ddbd-f892-29c8-733d-df3772dd4309>
2013-11-26 15:09:14 +0000

    Agregada interfaz Dificultad y clases Facil, Dificil, Moderado. Falta combinarla con GPS

---
+++ trunk/src/fiuba/algo3/modelo/Dificultad.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.modelo;
+
+public interface Dificultad {
+	
+	public int getFilas();
+	public int getColumnas();
+	public int getMultiplicador();
+	public int getMaximoDeMovimientos();
+	
+}
